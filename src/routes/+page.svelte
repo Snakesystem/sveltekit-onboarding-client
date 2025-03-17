704478d09ee437249a24547f5b592b7b5fb579e0
feryirawan <ir15y4hh@gmail.com>
2025-03-17 22:02:47 +0700

    Push push

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,29 +2,8 @@
   import { goto } from '$app/navigation';
   import { onMount } from 'svelte';
 
-  export let data;
-
-  async function logout() {
-    const res = await fetch("http://localhost:8000/api/v1/auth/logout", {
-      credentials: "include",
-      method: "POST",
-    });
-    const data = await res.json();
-    if (data.result) {
-      goto("/login");
-    }
-  }
-
   onMount(() => {
     goto("/cif");
   });
 
 </script>
-
-<h1>Profile Page</h1>
-<p>Nama Lengkap: {data.session?.fullname}</p>
-<p>Email: {data.session?.email}</p>
-<p>Referral ID: {data.session?.referral}</p>
-<p>Client Category: {data.session?.client_category}</p>
-
-<button on:click={logout}>Logout</button>
