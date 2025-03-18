60d90f1e64b17cb124e4c6d939e965e0b74aa34a
Snakesystem <ir15y4hh@gmail.com>
2025-03-19 04:54:06 +0700

    Add select custom

---
+++ src/components/SelectDynamic.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  const { viewData, placeholder, code = "", description = "" } = $props<{
+    viewData: string;
+    placeholder: string;
+    code?: string;
+    description?: string;
+  }>();
+  let options: { data_id: number; code: string; description: string }[] = $state([]);
+  let selectedValue: string = $state("");
+
+  // Fetch data dari API saat komponen dipasang
+  onMount(async () => {
+    try {
+      const res = await fetch(`http://localhost:8000/api/v1/option/${viewData}`);
+      const json = await res.json();
+
+      if (json.result) {
+        options = json.data;
+      } else {
+        console.error("Failed to fetch data:", json.message);
+      }
+    } catch (error) {
+      console.error("Error fetching options:", error);
+    }
+  });
+</script>
+
+<select class="form-select" bind:value={selectedValue}>
+  <option value="">{placeholder}</option>
+  {#each options as option}
+    <option value={option.data_id}>{option.description}</option>
+  {/each}
+</select>
+
+<style lang="scss" scoped>
+    .form-select {
+        font-size: 0.8rem;
+        option {
+            font-size: 0.8rem;
+        }
+
+        option[value=""] {
+            color: #999;
+        }
+    }
+    
+</style>
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,5 +1,36 @@
 <script>
+  import SelectDynamic from "../../components/SelectDynamic.svelte";
+
 
 </script>
 
-<h1>Register</h1>
+<h5>Register</h5>
+<p>Mohon untuk melengkapi informasi Data Pribadi berikut ini dengan data yang benar!</p>
+<form action="">
+    <div class="row">
+        <div class="col-12 mb-3">
+            <label for="full_name" class="form-label">Nama Lengkap (KTP)</label>
+            <input type="text" class="form-control" id="full_name">
+        </div>
+        <div class="col-12 col-lg-6 mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control" id="email">
+        </div>
+        <div class="col-12 col-lg-6 mb-3">
+            <label for="phone" class="form-label">No Telepon</label>
+            <input type="text" class="form-control" id="phone">
+        </div>
+        <div class="col-12 mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" class="form-control" id="password">
+        </div>
+        <div class="col-12 col-lg-6 mb-3">
+            <label for="sales" class="form-label">Sales</label>
+            <SelectDynamic viewData="sales" placeholder="Pilih Sales" />
+        </div>
+        <div class="col-12 mb-3">
+            <label for="password_confirmation" class="form-label">Konfirmasi Password</label>
+            <input type="password" class="form-control" id="password_confirmation">
+        </div>
+    </div>
+</form>
